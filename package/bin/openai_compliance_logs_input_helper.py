e667256b23e9768f27b91c8c689a15f9fe330128
Isaac Fonseca <ifonsecamonge@splunk.com>
2026-05-28 15:10:03 -0600

    fix: handle new conversations endpoint

--- package/bin/openai_compliance_logs_input_helper.py
+++ package/bin/openai_compliance_logs_input_helper.py
@@ -1,9 +1,8 @@
 import json
 from splunklib import modularinput as smi
-import requests
 
 from openai_helper import OpenAIHelper
-from openai_consts import OPENAI_COMPLIANCE_API_BASE_URL, LIST_FILES, GET_FILE_CONTENT
+from openai_consts import LIST_FILES
 
 
 def validate_input(definition: smi.ValidationDefinition):
@@ -54,78 +53,46 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
             log_files, last_end_time = helper.make_request(
                 api_key, workspace_id, endpoint_arg, params
             )
-            logger.debug(f"before: last_end_time: {last_end_time}")
 
             if last_end_time is not None and "Z" in last_end_time:
                 last_end_time = last_end_time.replace("Z", "")
 
-            logger.debug(f"after: last_end_time: {last_end_time}")
-
             if not log_files:
-                logger.info(f"No data was found for {endpoint_arg} at this time.")
+                logger.info(f"No data found for {endpoint_arg} at this time.")
                 return
 
-            is_checkpoint_saved = False
+            events = helper.get_log_files_content(api_key, workspace_id, log_files)
 
-            if log_files:
-                count = 0
-                for log_file in log_files:
-                    # Download the file
-                    log_file_id = log_file.get("id")
+            count = 0
 
-                    if not log_file_id:
-                        continue
+            for event in events:
+                event = json.loads(event)
+                logger.debug(f"Event: {type(event)} {event}")
 
-                    url = (
-                        OPENAI_COMPLIANCE_API_BASE_URL.format(workspace_id=workspace_id)
-                        + "/"
-                        + GET_FILE_CONTENT.format(log_file_id=log_file_id)
+                # Write the file content into Splunk
+                sourcetype = "openai:compliance:logs"
+
+                if event.get("type"):
+                    sourcetype = f"openai:compliance:{event.get('type').lower()}"
+
+                logger.debug(f"Sourcetype: {sourcetype}")
+
+                event_writer.write_event(
+                    smi.Event(
+                        data=json.dumps(event),
+                        index=input_item.get("index"),
+                        sourcetype=sourcetype,
                     )
-
-                    headers = {
-                        "Authorization": f"Bearer {api_key}",
-                        "Content-Type": "application/json",
-                    }
-
-                    response = requests.get(url, params=params, headers=headers)
-
-                    logger.debug(f"response from request: {response}")
-
-                    response.raise_for_status()
-
-                    file_content = response.text
-
-                    events = file_content.splitlines()
-
-                    for event in events:
-                        event = json.loads(event)
-                        logger.debug(f"Event: {type(event)} {event}")
-
-                        # Write the file content into Splunk
-                        sourcetype = "openai:compliance:logs"
-
-                        if event.get("type"):
-                            sourcetype = (
-                                f"openai:compliance:{event.get('type').lower()}"
-                            )
-
-                        logger.debug(f"Sourcetype: {sourcetype}")
-
-                        event_writer.write_event(
-                            smi.Event(
-                                data=json.dumps(event),
-                                index=input_item.get("index"),
-                                sourcetype=sourcetype,
-                            )
-                        )
-
-                        count += 1
-
-                if not is_checkpoint_saved:
-                    # save last_id as checkpoint
-                    checkpoint.update(checkpoint_name, last_end_time)
-                logger.info(
-                    f"Execution completed. A total of {count} new events were ingested."
                 )
+
+                count += 1
+
+            checkpoint.update(checkpoint_name, last_end_time)
+
+            logger.debug(f"Last timestamp saved as checkpoint: {last_end_time}")
+
+            logger.info(
+                f"Execution completed. A total of {count} new events were ingested."
+            )
         except Exception as e:
             logger.error(f"An error has ocurred in stream_events - {e}")
